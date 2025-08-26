e4429d9d3a0e0ea7af421b334e196abf66a4ecc2
Adi Yohanes <adiyohanes19@gmail.com>
2025-08-26 11:37:41 +0700

    finish setup constant

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { gsap } from "gsap";
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
-        <div className="flex-center h-[100vh]">
-            <h1 className="text-3xl text-indigo-300 text-center w-full">Hello, GSAP!</h1>
-        </div>
+        <main>
+            <Navbar />
+        </main>
     )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { navLinks } from '../../constants'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <a href="#home" className='flex items-center gap-2'>
+                    <p>Velvet Pour</p>
+                </a>
+                <ul>
+
+                </ul>
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
